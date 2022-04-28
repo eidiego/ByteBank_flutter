53e94ce4ab72f9bc24a086f67da942a310f21252
Diego Rodrigues <diegokod@gmail.com>
2022-04-27 22:59:03 -0300

    Feature: Transfers method add

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,10 @@ class BytebankApp extends StatelessWidget {
 }
 
 class TransferForm extends StatelessWidget {
+  final TextEditingController _controllerFieldNumberAcc =
+      TextEditingController();
+  final TextEditingController _controllerFieldValue = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,31 +30,48 @@ class TransferForm extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
-            const TextField(
-              style: TextStyle(
-                fontSize: 24.0,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _controllerFieldNumberAcc,
+                style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: const InputDecoration(
+                  labelText: 'Número da Conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                labelText: 'Numero da conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
             ),
-            TextField(
-              style: TextStyle(
-                fontSize: 24.0,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _controllerFieldValue,
+                style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: const Text('confirmar'),
-            ),
+              onPressed: () {
+                final int? numeroConta =
+                    int.tryParse(_controllerFieldNumberAcc.text);
+                final double? valor =
+                    double.tryParse(_controllerFieldValue.text);
+                if (numeroConta != null && valor != null) {
+                  final createdTransfer = Transfers(valor, numeroConta);
+                  debugPrint('$createdTransfer');
+                }
+              },
+              child: const Text('Confirmar'),
+            )
           ],
         ),
       ),
@@ -101,4 +122,9 @@ class Transfers {
   final int numeroConta;
 
   Transfers(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transfers{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
